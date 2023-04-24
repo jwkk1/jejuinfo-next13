d7f5430c9c75c7354a81fbcbc82be71cb67f54f0
jwkk1 <dog107.2@gmail.com>
2023-04-24 16:28:02 +0900

    feat: detailPage ui작업

--- app/search/page.jsx
+++ app/search/page.jsx
@@ -3,8 +3,44 @@ import List from "@/components/itemList/list";
 export default function SearchList() {
     return (
         <>
-            <h1>searchlist</h1>
-            <List />
+        <section className="text-gray-600 body-font">
+          <div className="container px-5 py-24 mx-auto">
+            <div className="flex flex-wrap w-full mb-20">
+              <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
+                <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">여행지 검색</h1>
+                <div className="h-1 w-20 bg-indigo-500 rounded"></div>
+              </div>
+              {/* <p className="lg:w-1/2 w-full leading-relaxed text-gray-500">카테고리</p> */}
+            </div>
+            <div class="flex mx-auto flex-wrap mb-20">
+                <a class="bg-gray-100 border-indigo-500 text-indigo-500 mr-5 sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium inline-flex items-center leading-none tracking-wider rounded-t">
+                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5 mr-3" viewBox="0 0 24 24">
+                    <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
+                    </svg>관광지
+                </a>
+                <a class="mr-5 sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium inline-flex items-center leading-none border-gray-200 hover:text-gray-900 tracking-wider">
+                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5 mr-3" viewBox="0 0 24 24">
+                    <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+                    </svg>쇼핑
+                </a>
+                <a class="mr-5 sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium inline-flex items-center leading-none border-gray-200 hover:text-gray-900 tracking-wider">
+                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5 mr-3" viewBox="0 0 24 24">
+                    <circle cx="12" cy="5" r="3"></circle>
+                    <path d="M12 22V8M5 12H2a10 10 0 0020 0h-3"></path>
+                    </svg>숙박
+                </a>
+                <a class="mr-5 sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium inline-flex items-center leading-none border-gray-200 hover:text-gray-900 tracking-wider">
+                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-5 h-5 mr-3" viewBox="0 0 24 24">
+                    <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
+                    <circle cx="12" cy="7" r="4"></circle>
+                    </svg>음식점
+                </a>
+            </div>
+            <div className="flex flex-wrap -m-4">
+                <List />
+            </div>
+          </div>
+        </section>
         </>
     );
 }
--- components/itemList/list.jsx
+++ components/itemList/list.jsx
@@ -1,12 +1,15 @@
 'use client'
 
 import { asyncSearchList } from "@/app/globalRedux/features/searchList/searchList";
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux"
 
 export default function List() {
     const dispatch = useDispatch();
-    const items = useSelector((state) => state.searchList)
+    const items = useSelector((state) => state.searchList);
+    const router = useRouter();
+
     console.log(items)
     useEffect(()=>{
         const params = {
@@ -17,9 +20,32 @@ export default function List() {
         }
         dispatch(asyncSearchList(params))
     },[])
+    if(items.value)
     return(
-        <div>
-            List
-        </div>
+        <>
+        {
+            items.value.map((item, i)=>{
+                let tag = item.alltag !== null ? item.alltag.split(',') : '';
+
+                if(tag || tag.length > 4){
+                    tag = tag.slice(0, 3);
+                    tag.forEach((item,i)=> tag[i]=`#${item}`)
+                }
+
+                return(
+                    <div className="xl:w-1/4 md:w-1/2 p-4" key={i} onClick={()=>{router.push('/detail')}}>
+                        <div className="bg-gray-100 p-6 rounded-lg" style={{minHeight : '25rem'}}>
+                            <img className="h-40 rounded w-full object-cover object-center mb-6" src={item.repPhoto.photoid.thumbnailpath} alt="content" />
+                            <h3 className="tracking-widest text-indigo-500 text-xs font-medium title-font">{tag}</h3>
+                            <h2 className="text-lg text-gray-900 font-medium title-font mb-4">{item.title}</h2>
+                            <p className="leading-relaxed text-base">{item.roadaddress}</p>
+                        </div>
+                    </div>
+                )
+            })
+        }
+
+        </>
+
     )
 }
